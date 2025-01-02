7c5ccbb9efb2d2dbfc0123bd128c2d0897c56251
KARLAFAY\w1120 <w112005266@gmail.com>
2025-01-02 13:16:35 +0800

    外送員部分有些檔案沒push到 我用zip檔內文件幫忙更新了

--- app/__init__.py
+++ app/__init__.py
@@ -26,12 +26,14 @@ def create_app():
     from app.controllers.restaurant import restaurant_bp
     from app.controllers.order import order_bp
     from app.controllers.auth_controller import auth_blueprint
+    from app.controllers.delivery_person import delivery_bp
     from app.controllers.cus_controller import customer_blueprint
     app.register_blueprint(main_blueprint)
     app.register_blueprint(user_bp, url_prefix='/users')
     app.register_blueprint(restaurant_bp, url_prefix='/restaurant')
     app.register_blueprint(order_bp, url_prefix='/orders')
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
+    app.register_blueprint(delivery_bp, url_prefix='/delivery')
     app.register_blueprint(customer_blueprint, url_prefix='/customer')
 
     return app
